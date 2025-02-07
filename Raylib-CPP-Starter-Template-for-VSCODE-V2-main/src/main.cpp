7354b5cc8edadd0e66839092f3e937455fc0cf5b
SoyNelly <wasnelly4@gmail.com>
2025-02-06 20:11:24 -0500

    Scoring system and Player Objects

--- Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
+++ Raylib-CPP-Starter-Template-for-VSCODE-V2-main/src/main.cpp
@@ -1,6 +1,30 @@
 #include<raylib.h>
 
+typedef struct Player{
+
+    float x;
+    float y;
+
+    int width;
+    int height;
+
+    float speed;
+
+    Rectangle rec;
+
+} player_t;
+
+typedef struct Ball{
+
+    int radius;
+
+
+} ball_t;
+
+
 int main(){
+
+    Vector2 center = {20.00, 10.0};
 
     Color green = {20, 160, 133, 255};
 
@@ -12,6 +36,10 @@ int main(){
 
     int ballX = 400;
     int ballY = 400;
+
+    int p1Score = 0;
+    int p2Score = 0;
+
     InitWindow(800, 800, "Poing!");
     SetTargetFPS(60);
 
@@ -20,61 +48,48 @@ int main(){
 
         ClearBackground(green);
 
-        // if (IsKeyDown('W')){
-        //     playerOneY -= 5;   
-        // }
-        // else if (IsKeyDown('S')){
-        //     playerOneY += 5;
-        // }
-        // else if (IsKeyDown(KEY_UP)){
-        //     playerTwoY -= 5;
-        // }
-        // else if (IsKeyDown(KEY_DOWN)){
-        //     playerTwoY += 5;
-        // }
-        
         DrawRectangle(playerOneX, playerOneY, 25, 75, WHITE);
         DrawRectangle(playerTwoX, playerTwoY, 25, 75, WHITE);
 
         DrawCircle(ballX, ballY, 20, WHITE);
 
+
+        // Player movement
         if (IsKeyDown('W')){
-            ballY -= 5;   
+            playerOneY -= 5;   
         }
         else if (IsKeyDown('S')){
-            ballY += 5;
+            playerOneY += 5;
         }
-        else if (IsKeyDown('A')){
-            ballX -= 5;
+        else if (IsKeyDown(KEY_UP)){
+            playerTwoY -= 5;
         }
-        else if (IsKeyDown('D')){
-            ballX += 5;
+        else if (IsKeyDown(KEY_DOWN)){
+            playerTwoY += 5;
         }
-        
+    
+        // Borders and stuff
         if (ballX >= 775 && ballY >= 0){
             ballX = 400;
             ballY = 400;
-            
+            p1Score++;
         }
-        else if (ballX <= 0 && ballY <= 0)
-        {
+        else if (ballX <= 0 && ballY <= 0){
             ballX = 400;
             ballY = 400;
+            p2Score++;
         }
-        else if (ballX <= 25 && ballY <= 775)
-        {
+        else if (ballX <= 25 && ballY <= 775){
             ballX = 400;
             ballY = 400;
-
-
+            p2Score++;
         }
-        else if (ballX >= 775 && ballY <= 775)
-        {
+        else if (ballX >= 775 && ballY <= 775){
             ballX = 400;
             ballY = 400;
+            p1Score++;
         }
-        else if (ballY <= 25)
-        {
+        else if (ballY <= 25){
             ballX = 400;
             ballY = 400;
         }
@@ -83,11 +98,10 @@ int main(){
             ballY = 400;
         }
         
+        DrawText(TextFormat("%i | %i", p1Score, p2Score), 300, 15, 100, RED);
         
-        
-        
-        
-        
+//        if(CheckCollisionCircleRec(center, radius, ))
+
         EndDrawing();
     }
     
